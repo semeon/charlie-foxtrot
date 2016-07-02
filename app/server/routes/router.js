f595679df5059d0ef078e7e797ef68867e09a289
semeon <semeon.peradze@gmail.com>
2016-07-01 22:41:11 -0700

    router debug

--- app/server/routes/router.js
+++ app/server/routes/router.js
@@ -8,12 +8,16 @@ class Router {
 	}
 	
 	returnSprintList(req, res, next) {
+	  console.log("SERVER: Route requested: " + req.route.path);
+	  console.log("SERVER: URL requested: " + req.url);
 		var list = this.model.getSprintList();
 	  res.send(list);
 	  return next();	
 	}
 
 	returnSprintById(req, res, next) {
+	  console.log("SERVER: Route requested: " + req.route.path);
+	  console.log("SERVER: URL requested: " + req.url);
 		var id = req.params.id;
 		var result = this.model.getSprintById(id);
 	  res.send(result);
@@ -21,6 +25,8 @@ class Router {
 	}
 
 	startDataUpdate(req, res, next) {
+	  console.log("SERVER: Route requested: " + req.route.path);
+	  console.log("SERVER: URL requested: " + req.url);
 		this.retriever.start();
 	  res.send("Data update initiated. Might take a while.");
 	  console.log("Calling data retriever..");
